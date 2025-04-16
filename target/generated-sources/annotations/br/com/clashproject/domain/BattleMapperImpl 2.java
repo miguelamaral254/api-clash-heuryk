78b74dc886be51e6dd4b9b55b8159791a4b2a403
miguelamaral254 <miguel.amaral.sales@gmail.com>
2025-04-16 19:54:25 -0300

    modelagem

--- target/generated-sources/annotations/br/com/clashproject/domain/BattleMapperImpl 2.java
+++ target/generated-sources/annotations/br/com/clashproject/domain/BattleMapperImpl 2.java
@@ -21,7 +21,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-16T13:15:48-0300",
+    date = "2025-04-16T19:53:19-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
 )
 @Component
